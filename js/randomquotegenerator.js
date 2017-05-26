7432fef3c7daa4b1d7034163146f6fbff455e8c2
Phil Fevry <phil.fevry@gmail.com>
2017-05-25 21:10:27 -0400

    connected to api and made design tweaks

---
+++ js/randomquotegenerator.js
@@ -0,0 +1,34 @@
+var QuoteGenerator, Randomizer;
+
+QuoteGenerator = {
+	objects: {
+		quote: $('#QuoteText'),
+		glyph: $('#QuoteGlyph'),
+		author: $('#QuoteAuthor'),
+		retriever: $('#QuoteRetriver')
+	},
+
+	init: function() {
+		sr = this.objects;
+		sr.retriever.on('click', this.getNew);
+		this.getNew();
+	},
+
+	getNew: function() {
+		QuoteGenerator.objects.quote.text('Loading...');
+		QuoteGenerator.objects.author.text('');
+		QuoteGenerator.objects.glyph.css('visibility', 'hidden');
+		$.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+			function(data) {
+				var q = QuoteGenerator.objects;
+				q.quote.text(data.quoteText);
+				q.author.text('- ' + data.quoteAuthor);
+				q.glyph.css('visibility', 'visible');
+		});
+	}
+};
+
+$(function() {
+	QuoteGenerator.init();
+});
+
