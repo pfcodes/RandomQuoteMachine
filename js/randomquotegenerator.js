0cc71d0681cf8b315895e340637af3d2be9a8f47
Phil Fevry <phil.fevry@gmail.com>
2017-06-12 21:00:06 -0400

    fixed indentation

--- js/randomquotegenerator.js
+++ js/randomquotegenerator.js
@@ -1,36 +1,36 @@
 'use strict'
 
 var QuoteGenerator, 
-	Randomizer	// TODO: code this
+  Randomizer  // TODO: code this
 
 QuoteGenerator = {
-	objects: {
-		quote: $('#QuoteText'),
-		glyph: $('#QuoteGlyph'),
-		author: $('#QuoteAuthor'),
-		retriever: $('#QuoteRetriver')
-	},
+  objects: {
+    quote: $('#QuoteText'),
+    glyph: $('#QuoteGlyph'),
+    author: $('#QuoteAuthor'),
+    retriever: $('#QuoteRetriver')
+  },
 
-	init: function() {
-		sr = this.objects
-		sr.retriever.on('click', this.getNew)
-		this.getNew()
-	},
+  init: function() {
+    sr = this.objects
+    sr.retriever.on('click', this.getNew)
+    this.getNew()
+  },
 
-	getNew: function() {
-		QuoteGenerator.objects.quote.text('Loading...')
-		QuoteGenerator.objects.author.text('')
-		QuoteGenerator.objects.glyph.css('visibility', 'hidden')
-		$.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
-			function (data) {
-				var q = QuoteGenerator.objects
-				q.quote.text(data.quoteText)
-				q.author.text('- ' + data.quoteAuthor)
-				q.glyph.css('visibility', 'visible')
-		})
-	}
+  getNew: function() {
+    QuoteGenerator.objects.quote.text('Loading...')
+    QuoteGenerator.objects.author.text('')
+    QuoteGenerator.objects.glyph.css('visibility', 'hidden')
+    $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+      function (data) {
+        var q = QuoteGenerator.objects
+        q.quote.text(data.quoteText)
+        q.author.text('- ' + data.quoteAuthor)
+        q.glyph.css('visibility', 'visible')
+    })
+  }
 }
 
 $(function() {
-	QuoteGenerator.init()
+  QuoteGenerator.init()
 })
